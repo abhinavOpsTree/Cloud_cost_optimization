1a515243e3e10e41f07c8546b2bea077020e5a67
abhinavOpsTree <abhinav.rai@opstree.com>
2026-09-14 10:47:34 +0530

    Delete Cloud_Sentry-main directory

--- Cloud_Sentry-main/tests/test_df_cols.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-# Data now loaded live via UnitEconPro API - see core/uniteconpro_client.py
-# df = pd.read_excel(...)  # Legacy static file approach removed
-print(df.columns.tolist())
